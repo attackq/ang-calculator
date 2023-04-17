6444d4b973adabeaf62c43a6b928a8888f4e2825
Alexander Panasik <rastpk@gmail.com>
2023-04-17 22:54:53 +0300

    html edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ang-calculator';
+
+  public calValue: number = 0;
+  public funcT: any = 'NoFunction';
+
+  onClickValue(val: string, type: any) {
+    console.log(val, type);
+  }
 }
